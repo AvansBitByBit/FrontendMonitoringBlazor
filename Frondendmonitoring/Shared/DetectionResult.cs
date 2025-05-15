c7eebf7e77a87bdc39fe4932c25e0ad769e8ded8
Yazan26 <yazandawud75@gmail.com>
2025-05-15 14:56:28 +0200

    added favicon & example class

---
+++ Frondendmonitoring/Shared/DetectionResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Frondendmonitoring.Shared
+{
+
+        
+    public class DetectionResult
+    {
+        public string Type { get; set; }
+        public float Confidence { get; set; }
+        public string Location { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
