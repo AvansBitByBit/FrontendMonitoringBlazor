c96afe2585e05521bf7907eaa390c437d70215c5
Yazan <yazandawud75@gmail.com>
2025-06-04 16:51:08 +0200

    Refactor HTTP and auth

--- FrontendMonitoring/Services/WeatherApiClient.cs
+++ FrontendMonitoring/Services/WeatherApiClient.cs
@@ -1,15 +1,18 @@
-﻿using FrontendMonitoring.Models;
+using FrontendMonitoring.Models;
+using FrontendMonitoring.Services;
+
 public class WeatherApiClient
 {
-    private readonly HttpRequesterOnlyUrl.HttpRequesterOnlyUrl _httpRequesterOnlyUrl;
+    private readonly ApiClient _apiClient;
 
-    public WeatherApiClient(HttpRequesterOnlyUrl.HttpRequesterOnlyUrl httpRequesterOnlyUrl)
+    public WeatherApiClient(ApiClient apiClient)
     {
-        _httpRequesterOnlyUrl = httpRequesterOnlyUrl;
+        _apiClient = apiClient;
     }
 
     public Task<WeatherModel?> GetDataAsync()
     {
-        return _httpRequesterOnlyUrl.GetAsync<WeatherModel>("https://api.open-meteo.com/v1/forecast?latitude=51.571915&longitude=4.768323&hourly=temperature_2m,rain&current=temperature_2m&timezone=auto&past_days=7");
+        return _apiClient.GetAsync<WeatherModel>(
+            "https://api.open-meteo.com/v1/forecast?latitude=51.571915&longitude=4.768323&hourly=temperature_2m,rain&current=temperature_2m&timezone=auto&past_days=7");
     }
 }
